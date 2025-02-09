e6c801678ea7132c3ed35c2b6760fe77e59a953d
jon ander <jonander2233@gmail.com>
2025-02-09 13:37:31 +0100

    bug fix in menu

--- src/main/java/org/jonander2233/lib_personal/Menu.java
+++ src/main/java/org/jonander2233/lib_personal/Menu.java
@@ -13,46 +13,47 @@ public class Menu {
         this.mensajes = ResourceBundle.getBundle("messages", locale);
 
     }
+    /**
+     * Muestra un menu con las opciones pasadas por parametro con opcion de salir
+     * @param titulo
+     * @param opciones
+     * @return
+     */
 
     public int show(String titulo, String[] opciones,String textoFinal){
         int opcion;
-        do {
-            System.out.println("*********************");
-            System.out.println("****" + titulo + "****");
-            System.out.println("*********************");
-            for (int i = 0; i < opciones.length; i++) {
-                System.out.println(i + 1 + "." + opciones[i]);
-            }
-            System.out.println("-----------------------");
-            System.out.println("0."+textoFinal);
-            opcion = validarOpcion();
-            if (opcion < 0 || opcion > opciones.length) {
-                System.out.println(mensajes.getString("num_invalido_err"));
-            }
-        }while (opcion < 0 || opcion > opciones.length);
+        System.out.println("*********************");
+        System.out.println("****" + titulo + "****");
+        System.out.println("*********************");
+        for (int i = 0; i < opciones.length; i++) {
+            System.out.println(i + 1 + "." + opciones[i]);
+        }
+        System.out.println("-----------------------");
+        System.out.println("0."+textoFinal);
+        opcion = validarOpcion(0,opciones.length);
         return opcion;
     }
+
+    /**
+     * Muestra un menu con las opciones pasadas por parametro sin opcion de salir
+     * @param titulo
+     * @param opciones
+     * @return
+     */
     public int show(String titulo, String[] opciones){
         int opcion;
-        do {
-            System.out.println("***************************************");
-            System.out.println("****" + titulo + "****");
-            System.out.println("***************************************");
-            for (int i = 0; i < opciones.length; i++) {
-                System.out.println(i + 1 + "." + opciones[i]);
-            }
-            System.out.println("-----------------------");
-            opcion = validarOpcion();
-
-            if (opcion < 1 || opcion > opciones.length) {
-                System.out.println(mensajes.getString("num_invalido_err"));
-            }
-
-        }while (opcion < 1 || opcion > opciones.length);
+        System.out.println("***************************************");
+        System.out.println("****" + titulo + "****");
+        System.out.println("***************************************");
+        for (int i = 0; i < opciones.length; i++) {
+            System.out.println(i + 1 + "." + opciones[i]);
+        }
+        System.out.println("-----------------------");
+        opcion = validarOpcion(1,opciones.length);
         return opcion;
     }
 
-    private static int validarOpcion(){
+    private static int validarOpcion(int min, int max){
         Scanner scanner = new Scanner(System.in);
         int numero=0;
         boolean valido=false;
@@ -60,7 +61,11 @@ public class Menu {
             try {
                 scanner.hasNextInt();
                 numero = scanner.nextInt();
-                valido = true;
+                if (numero < min || numero > max) {
+                    System.out.println(mensajes.getString("num_invalido_err"));
+                }else {
+                    valido = true;
+                }
             }catch (Exception e){
                 System.out.println(mensajes.getString("int_num_err"));
                 scanner.next();
